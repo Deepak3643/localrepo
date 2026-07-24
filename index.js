d4b1350afc536085e26a8a8f8384fac59c294fe9
Deepak Kumar <deepakkr3643@gmail.com>
2026-07-24 15:46:08 +0530

    fixed prompt menu

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log("Hello World");
 console.log("This is a sample code snippet from LocalRepo/index.js");
 
-let a = 5;
-console.log(a);
-
+const btn = document.getElementById("prompt-btn");
+    const output = document.getElementById("output");
+    btn.addEventListener("click", () => {
+      const userName = window.prompt("What is your name?", "");
+      output.textContent = "Hello, " + userName + "!";
+    });
 
